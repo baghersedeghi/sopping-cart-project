c5c12c6a91ec506802c11b1ea4d3da38bb001c3e
baghersedeghi <109987141+baghersedeghi@users.noreply.github.com>
2026-09-12 21:18:21 +0330

    refactor: improve app structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// import React from 'react'
-
-import { BrowserRouter, Route, Routes } from "react-router"
-import Home from "./pages/Home"
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { ToastContainer } from 'react-toastify';
-import ProductList from "./conponents/ProductList";
-import Header from "./conponents/Header";
-import Cart from "./pages/Cart";
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+
 function App() {
-   return (
-      <>
-         <BrowserRouter>
-            <Header></Header>
-            <Routes>
-               <Route path="/" element={<Home />}></Route>
-               <Route path="/products" element={<ProductList />}></Route>
-               <Route path="/cart" element={<Cart />}></Route>
-            </Routes>
-            <ToastContainer position='top-center' />         </BrowserRouter>
-      </>
-   )
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </main>
+      <ToastContainer position="top-center" />
+    </BrowserRouter>
+  );
 }
 
-export default App  
+export default App;
